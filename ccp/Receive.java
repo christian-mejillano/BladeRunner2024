3e0e952c5061071f6a6e5c3c890eb4c40b33658d
AJW737 <88125403+AJW737@users.noreply.github.com>
2024-08-28 17:04:21 +1000

    Edit Send and Receive

---
+++ ccp/Receive.java
@@ -0,0 +1,11 @@
+
+import java.io.IOException;
+import java.net.DatagramSocket; 
+public class Receive {
+
+     public static void main(String[] args) throws IOException 
+    { 
+    DatagramSocket ds = new DatagramSocket(1234); 
+    byte[] receiveData = new byte[1024];
+    }
+}
---
+++ ccp/Send.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+import java.net.DatagramSocket;
+
+public class Send {
+     public static void main(String[] args) throws IOException 
+    { 
+   DatagramSocket socket = new DatagramSocket();
+
+    }
+}
