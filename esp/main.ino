8b0e9fe3b716734069a8ae79730bf57145f528c4
oturner01 <126745329+oturner01@users.noreply.github.com>
2024-09-04 09:45:12 +1000

    Update main.ino

--- esp/main.ino
+++ esp/main.ino
@@ -1,7 +1,18 @@
-void setup() {
+// ESP32 WiFi connection to Arduino
 
+#include "WiFi.h"
+
+const char* ssid = "networkName";
+const char* password = "networkPassword";
+ 
+void setup() {
+	
+	Serial.begin(115200);
+	WiFi.begin(ssid, password);
+
+	
 }
 
 void loop() {
-    
+
 }
